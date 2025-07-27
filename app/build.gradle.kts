3c09787b6e8b577effe29fe63ea1500cb126f491
owo233 <ver@vip.qq.com>
2025-07-27 14:07:30 +0800

    build: 更新项目配置和依赖版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
@@ -40,8 +41,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
 
     packaging {
