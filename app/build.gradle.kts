9e982f3e99fdc3565d7d79fe86d2ff8cff65a9bf
owo233 <ver@vip.qq.com>
2025-08-02 03:14:24 +0800

    bump version 1.2.3-fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.owo233.fuckmarketads"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.2.3"
+        versionCode = 21
+        versionName = "1.2.3-fix"
         buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
     }
 
